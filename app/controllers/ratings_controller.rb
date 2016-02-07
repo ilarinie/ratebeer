20484072a8346fc0d94913436e2b8a3853f32f7d
Ilari <ilari.nieminen@outlook.com>
2016-02-07 17:59:41 +0200

    viikko 3

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,14 +10,21 @@ class RatingsController < ApplicationController
   
   
   def create
-    Rating.create params.require(:rating).permit(:score, :beer_id)
-	redirect_to ratings_path
+    @rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+	
+	if @rating.save
+		current_user.ratings << @rating
+		redirect_to user_path current_user
+	else
+		@beers = Beer.all
+		render :new
+	end
   end
 
   def destroy
-	rating = Rating.find(params[:id])
-	rating.delete
-	redirect_to ratings_path
+    rating = Rating.find params[:id]
+    rating.delete if current_user == rating.user
+    redirect_to :back
   end
   
 end
